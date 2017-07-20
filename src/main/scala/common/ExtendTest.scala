11f1d87732f10d2e9b8c8293d7e4bd17fc060223
zing <578893415@qq.com>
2017-07-20 17:13:21 +0800

    Initial commit

--- src/main/scala/common/ExtendTest.scala
+++ src/main/scala/common/ExtendTest.scala
@@ -3,6 +3,27 @@ package common
 /**
   * Created by Zing on 2017/7/20.
   */
+
+class House(val address: String, val state: String, val zip: String) {
+  def this(state: String, zip: String) =
+    this("address?", state, zip)
+
+  def this(zip: String) =
+    this("address?", "state?", zip)
+}
+
+class Home(address: String, state: String, zip: String, val name: String)
+  extends House(address, state, zip) {
+  override def toString: String = s"$name:$address,$state,$zip"
+}
+
+class VacationHouse(state: String, zip: String, val startMonth: Int, val endMonth: Int)
+  extends House(state, zip)
+
+class TreeHouse(val name: String, zip: String)
+  extends House(zip)
+
 object ExtendTest {
+
 
 }
